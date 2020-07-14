b6a7bbc8bdc8e88441ec0eb939d47bda1aed3f96
CodeIsJustLikeMagic <CodeIsJustLikeMagic@web.de>
2020-07-14 13:42:13 +0200

    remaned project. show json response from azure

---
+++ MagicLeapObjectDetection/Assets/Scripts/AzureTester.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class AzureTester : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown("t"))
+        {
+            Debug.Log("testing image");
+            string filename = "C:/ObjectDetectionForAR/MagicLeapObjectDetection/Assets/TestImages/IMG_20200702_153228.jpg";
+            
+            //string.Format(@"CapturedImage{0}.jpg", tapsCount);
+
+            string filePath = Path.Combine(Application.persistentDataPath, filename);
+            Debug.Log(filePath);
+            StartCoroutine(VisionManager.instance.AnalyseImageFromFile(filename));
+        }
+    }
+}
---
+++ MagicLeapObjectDetection/Assets/Scripts/ResultAsText.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResultAsText : MonoBehaviour
+{
+    public static ResultAsText instance;
+    private Text output;
+    private void Awake()
+    {
+        instance = this;
+    }
+    void Start()
+    {
+        output = GameObject.FindObjectOfType<Text>();
+    }
+
+    public void show()
+    {
+        output.text = "hello, its me!";
+    }
+}
